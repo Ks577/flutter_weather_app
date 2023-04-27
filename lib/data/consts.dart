429f530f9bf969d56211460412484ab4e4da56dd
kseniia <91552596+Ks577@users.noreply.github.com>
2023-04-27 16:01:59 +0300

    Delete consts.dart

--- lib/data/consts.dart
+++
@@ -1,8 +0,0 @@
-abstract class Consts {
-  static const baseUrl = 'https://api.openweathermap.org/data/2.5/';
-  static const openWeatherMapKey = 'API_KEY';
-  static const getWeatherRequest = 'weather';
-  static const hotTemperature = 25;
-  static const warmTemperature = 15;
-  static const coldTemperature = 0;
-}
